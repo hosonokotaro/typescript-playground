0a114fc39e801fe25ed6a0a70edd4ede496dc4c4
hosono kotaro <hosono1985@gmail.com>
2021-05-15 21:01:56 +0900

    feat: Add sample code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log('hello');
+// Sample code
+
+type Position = 'Manager' | 'Chief' | 'Employee';
+
+interface TeamMember {
+  id: number;
+  name: string;
+  position: Position;
+}
+
+const data: TeamMember[] = [
+  {
+    id: 1,
+    name: 'yamada',
+    position: 'Manager',
+  },
+  {
+    id: 2,
+    name: 'suzuki',
+    position: 'Employee',
+  },
+  {
+    id: 3,
+    name: 'tanaka',
+    position: 'Employee',
+  },
+];
+
+const showTeamMember = (data: TeamMember[]) => {
+  data.map((member) => {
+    console.log(member);
+  });
+};
+
+showTeamMember(data);
