ab8ffd190ed12b873778937f44a3260a4f0fb39d
hosono kotaro <hosono1985@gmail.com>
2021-05-23 12:27:32 +0900

    feat: Add type test, Object.keys

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,46 @@
-import { data, TeamMember } from './data';
+// import { data, TeamMember } from './data';
 
-const showTeamMember = (memberList: TeamMember[]) => {
-  memberList.map(({ id, name, position }) => {
-    console.log(`id: ${id} | name: ${name} | position: ${position}`);
-  });
-};
+// const showTeamMember = (memberList: TeamMember[]) => {
+//   memberList.map(({ id, name, position }) => {
+//     console.log(`id: ${id} | name: ${name} | position: ${position}`);
+//   });
+// };
 
-showTeamMember(data);
+// showTeamMember(data);
 
-// Generics test
+// NOTE: Generics test
+// NOTE: 0x から始まるものは16進数を表す
+// NOTE: Symbol 型は文字列に変換できないので、表示したい場合は .toString() で変換する
 
-// Arrow function には = <T>(xxx: T): T というように Generics を定義する
+// NOTE: Arrow function には = <T>(xxx: T): T というように Generics を定義する
 const showManyList = <T>(list: T[]) => {
   list.map((item) => {
-    if (typeof item === 'number') console.log(item + 10);
-    if (typeof item === 'string') console.log(`${item}です`);
+    console.log(`${typeof item}`);
   });
 };
 
-showManyList(['aaa', 'bbb', 'ccc']);
-showManyList([1, 2, 3]);
+showManyList([
+  1,
+  0.1,
+  0xff88,
+  NaN,
+  'fd',
+  false,
+  {},
+  null,
+  undefined,
+  Symbol('Sym'),
+  () => {},
+]);
+
+// NOTE: Object.keys に null, undefined を渡すと Error、string を渡すと文字数が返ってくる
+// symbol, number は [] が返ってくる。array は index が返ってくる
+const showKeys = (props: object) => {
+  const value = Object.keys(props);
+  console.log(value.length);
+};
+
+showKeys({ test1: 0, test2: { test3: 2, test4: 3 } });
+
+console.log({} === {});
+console.log(null === null);
